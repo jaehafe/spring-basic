d0d85aed0048b27594f2fa5365ff9dc448f99af8
jaeha <wogkdkrm112@gmail.com>
2023-10-06 08:55:06 +0900

    feat: RequestMappingTest

--- spring-basic/src/main/java/com/example/springbasic/ch02/RegisterController.java
+++ spring-basic/src/main/java/com/example/springbasic/ch02/RegisterController.java
@@ -1,17 +1,30 @@
 package com.example.springbasic.ch02;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URLEncoder;
 
 @Controller
 public class RegisterController {
-    @RequestMapping("/register/add")
+//    @RequestMapping("/register/add")
+    @GetMapping("/register/add")
     public String register() {
         return "registerForm"; // WEB-INF/views/registerForm.jsp
     }
 
-    @RequestMapping("/register/save")
-    public String save() {
+    @PostMapping("/register/save")
+//    @RequestMapping(value = "/register/save", method = RequestMethod.POST)
+    public String save(@ModelAttribute("user") User user, Model m) {
+        // 1. 유효성 검사
+//        if(!isValid(user)) {
+//            String msg = URLEncoder.encode("id를 잘못입력하셨습니다.", "utf-8");
+//
+//            m.addAttribute("msg", msg);
+//            return "redirect:/register/add"; // 신규회원 가입화면으로 이동(redirect)
+//        }
+
         return "registerInfo";
     }
 }
---
+++ spring-basic/src/main/java/com/example/springbasic/ch02/User.java
@@ -0,0 +1,61 @@
+package com.example.springbasic.ch02;
+
+public class User {
+    private String id;
+    private String pwd;
+    private String name;
+    private String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSns() {
+        return sns;
+    }
+
+    public void setSns(String sns) {
+        this.sns = sns;
+    }
+
+    private String sns;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", pwd='" + pwd + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", sns='" + sns + '\'' +
+                '}';
+    }
+}
